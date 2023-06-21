29ad5cb5b84a7b6372fbb91dbb5bb3655bee0487
rmora101 <59456883+rmora101@users.noreply.github.com>
2023-06-21 14:15:22 -0700

    added api,axios, and delete axios

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
 import React from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
-import  { useState } from 'react';
+import  { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const TASKS = [
-  {
-    id: 1,
-    title: 'Mow the lawn',
-    isComplete: false,
-  },
-  {
-    id: 2,
-    title: 'Cook Pasta',
-    isComplete: true,
-  },
-];
+
+// const TASKS = [
+//   {
+//     id: 1,
+//     title: 'Mow the lawn',
+//     isComplete: false,
+//   },
+//   {
+//     id: 2,
+//     title: 'Cook Pasta',
+//     isComplete: true,
+//   },
+// ];
 
 const App = () => {
-  const [tasksData, setTaskData] = useState(TASKS);
+  const [tasksData, setTaskData] = useState([]);
+    const API = "https://task-list-api-c17.onrender.com/tasks";
+
+  useEffect(() => {
+    axios
+      .get(API)
+      .then((result) => {
+        console.log(result.data);
+        setTaskData(result.data);
+      })
+      .catch((err)=> {
+        console.log(err);
+      });
+  }, []);
+
+
 
   const updateTaskData = (taskId) => {
     console.log('UpdateTaskData is being called');
     const newTasks = tasksData.map(task => {
       if (task.id === taskId) {
+        console.log(task.isComplete);
         return {...task, isComplete: !task.isComplete};
       } else {
         return {...task};
@@ -31,6 +49,20 @@ const App = () => {
     setTaskData(newTasks);
   };
   
+  const deleteTask = (id) => {
+    axios
+      .delete(`${API}/${id}`)
+      .then((result) => {
+        console.log(result.data);
+        const newTaskData = tasksData.filter(
+          (task) => task.id !== id 
+        );
+        setTaskData(newTaskData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   // const changeTaskData = (id, isComplete) => {
   //   const newTask = tasksData.map((task) => {
   //     if (task.id === id) {
@@ -47,12 +79,12 @@ const App = () => {
 
 
   // Copied from the live code in class 
-  const deleteTask = (id) => {
-    const newTaskData = tasksData.filter(
-      (task) => task.id !== id 
-    );
-    setTaskData(newTaskData);
-  };
+  // const deleteTask = (id) => {
+  //   const newTaskData = tasksData.filter(
+  //     (task) => task.id !== id 
+  //   );
+  //   setTaskData(newTaskData);
+  // };
 
 
   return (
